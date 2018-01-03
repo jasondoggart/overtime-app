6e0cbefaeccba0f856fef74458e3fe99ea00d95b
Jason Doggart <jasondoggart@gmail.com>
2018-01-03 13:00:56 -0500

    Update post index file with test

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,9 @@
+@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Job", last_name: "Snow")
+
+puts "1 user created"
+
 100.times do |post|
-  Post.create(date: Date.today, rationale: "#{post} rationale content")
+  Post.create(date: Date.today, rationale: "#{post} rationale content", user: @user)
 end
 
 puts "100 Posts have been created"
