ecc1227396a655160611714fc933f162ea62db91
Jason Doggart <jasondoggart@gmail.com>
2018-01-09 21:19:46 -0500

    block users from editing approved posts

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -1,5 +1,18 @@
 class PostPolicy < ApplicationPolicy
     def update?
-        user.admin? or record.user_id == user.id
+        return true if post_approved? and user.admin?
+        return true if user_or_admin and !post_approved?
+
+        
     end 
+
+    private
+
+        def user_or_admin
+            record.user == user || user.admin?
+        end
+
+        def post_approved?
+            record.status == 'approved'
+        end
 end
--- spec/features/approval_workflow_spec.rb
+++ spec/features/approval_workflow_spec.rb
@@ -27,5 +27,17 @@ describe 'navigate' do
 
         expect(page).to_not have_content('Approved')
        end
+
+       it 'should not be editable by post creator if status is approved' do
+        logout(:user)
+        user = FactoryBot.create(:user)
+        login_as(user, :scope => :user)
+
+        @post.update(user: user, status: 'approved')
+
+        visit edit_post_path(@post)
+
+        expect(current_path).to eq(root_path)
+       end
     end
 end
