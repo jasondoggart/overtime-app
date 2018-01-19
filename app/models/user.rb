25fbbb3b1cf8f0f0a1b2896483ffbeef95f45a4c
Jason Doggart <jasondoggart@gmail.com>
2018-01-19 17:15:33 -0500

    add user validation for phone

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   validates_presence_of :first_name, :last_name, :phone
+  validates :phone, length: { is: 10 }
+
+  PHONE_REGEX = /\A[0-9]*\Z/
+
+  validates_format_of :phone, with: PHONE_REGEX
 
   def full_name
     last_name.upcase + ", " + first_name.upcase
